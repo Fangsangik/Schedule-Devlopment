c8be875520cec770b05b30869cefd14d220dca04
Fangsangik <hwangsangik@gmail.com>
2024-11-12 21:26:41 +0900

    feat : "ScheduleMapper" - ScheduleMapper 통해 Dto와 Entity Mapping

---
+++ src/main/java/com/example/scheduledevelopment/schedule/mapper/ScheduleMapper.java
@@ -0,0 +1,21 @@
+package com.example.scheduledevelopment.schedule.mapper;
+
+import com.example.scheduledevelopment.member.mapper.MemberMapper;
+import com.example.scheduledevelopment.schedule.dto.ScheduleDto;
+import com.example.scheduledevelopment.schedule.entity.Schedule;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring", uses = {MemberMapper.class})
+public interface ScheduleMapper {
+    ScheduleMapper INSTANCE = Mappers.getMapper(ScheduleMapper.class);
+
+    @Mapping(source = "member", target = "memberDto")
+        // Entity -> Dto
+    ScheduleDto toDto(Schedule schedule);
+
+    @Mapping(source = "memberDto", target = "member")
+        // Dto -> Entity
+    Schedule toEntity(ScheduleDto scheduleDto);
+}
