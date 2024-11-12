889e224d3ce06694c69d1f9828cb7e128acaf43b
Fangsangik <hwangsangik@gmail.com>
2024-11-12 22:18:09 +0900

    refactor : "ScheduleDto" - comment List화 Mapping 추가

--- src/main/java/com/example/scheduledevelopment/schedule/mapper/ScheduleMapper.java
+++ src/main/java/com/example/scheduledevelopment/schedule/mapper/ScheduleMapper.java
@@ -1,21 +1,27 @@
 package com.example.scheduledevelopment.schedule.mapper;
 
+import com.example.scheduledevelopment.comment.mapper.CommentMapper;
 import com.example.scheduledevelopment.member.mapper.MemberMapper;
 import com.example.scheduledevelopment.schedule.dto.ScheduleDto;
 import com.example.scheduledevelopment.schedule.entity.Schedule;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = {MemberMapper.class})
+@Mapper(componentModel = "spring", uses = {MemberMapper.class, CommentMapper.class})
 public interface ScheduleMapper {
     ScheduleMapper INSTANCE = Mappers.getMapper(ScheduleMapper.class);
 
-    @Mapping(source = "member", target = "memberDto")
-        // Entity -> Dto
+    @Mappings({
+            @Mapping(source = "member", target = "memberDto"),
+            @Mapping(source = "comments", target = "commentDtoList")
+    }) // Entity -> Dto
     ScheduleDto toDto(Schedule schedule);
 
-    @Mapping(source = "memberDto", target = "member")
-        // Dto -> Entity
+    @Mappings({
+            @Mapping(source = "memberDto", target = "member"),
+            @Mapping(source = "commentDtoList", target = "comments")
+    }) // Dto -> Entity
     Schedule toEntity(ScheduleDto scheduleDto);
 }
