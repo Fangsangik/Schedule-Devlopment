6ec036fd38ef9ca88e12b20022f4a77570750f6d
Fangsangik <hwangsangik@gmail.com>
2024-11-12 21:24:54 +0900

    feat : "Comment" - 댓글 entity - id, 상세 댓글 - update 관련 기능 처리

---
+++ src/main/java/com/example/scheduledevelopment/comment/entity/Comment.java
@@ -0,0 +1,34 @@
+package com.example.scheduledevelopment.comment.entity;
+
+import com.example.scheduledevelopment.member.entity.Member;
+import com.example.scheduledevelopment.schedule.entity.Schedule;
+import com.example.scheduledevelopment.util.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String commentDetails;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public void updateCommentDetails(String commentDetails) {
+        this.commentDetails = commentDetails;
+    }
+
+    public void updateSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+}
