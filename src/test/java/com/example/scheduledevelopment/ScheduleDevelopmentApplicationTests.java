dea96dc3288ee0e24bf0acfaf97ae419a2e6b019
Fangsangik <hwangsangik@gmail.com>
2024-11-12 22:18:25 +0900

    init

---
+++ src/test/java/com/example/scheduledevelopment/ScheduleDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
