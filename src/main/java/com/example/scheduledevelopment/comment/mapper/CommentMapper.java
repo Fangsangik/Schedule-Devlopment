71691ed0601561de87f57e150f1b3c0161ce207d
Fangsangik <hwangsangik@gmail.com>
2024-11-12 22:13:48 +0900

    refactor : "CommentMapper" - 회원과 연관관계 Mapping, comment list mapping

--- src/main/java/com/example/scheduledevelopment/comment/mapper/CommentMapper.java
+++ src/main/java/com/example/scheduledevelopment/comment/mapper/CommentMapper.java
@@ -2,19 +2,27 @@ package com.example.scheduledevelopment.comment.mapper;
 
 import com.example.scheduledevelopment.comment.dto.CommentDto;
 import com.example.scheduledevelopment.comment.entity.Comment;
-import com.example.scheduledevelopment.schedule.mapper.ScheduleMapper;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
+
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = {ScheduleMapper.class})
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
     @Mapping(source = "schedule", target = "scheduleDto")
+    @Mapping(source = "member", target = "MemberDto")
     CommentDto toDto(Comment comment);
 
     @Mapping(source = "scheduleDto", target = "schedule")
+    @Mapping(source = "memberDto", target = "member")
     Comment toEntity(CommentDto commentDto);
+
+    List<CommentDto> toDtoList(List<Comment> comments);
+
+    List<Comment> toEntityList(List<CommentDto> commentDtos);
 }
