6551d3047b9abcb621606efab26d5e7549badc7c
Fangsangik <hwangsangik@gmail.com>
2024-11-12 21:25:49 +0900

    feat : "CommentDto" - dto

---
+++ src/main/java/com/example/scheduledevelopment/comment/dto/CommentDto.java
@@ -0,0 +1,23 @@
+package com.example.scheduledevelopment.comment.dto;
+
+import com.example.scheduledevelopment.member.dto.MemberDto;
+import com.example.scheduledevelopment.schedule.dto.ScheduleDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+    private Long id;
+    private String commentDetails;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private ScheduleDto scheduleDto;
+//    private MemberDto memberDto;
+}
