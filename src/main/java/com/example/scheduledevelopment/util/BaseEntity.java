f96b7c797d2b2cff2fd67b4c6ee0efebd6ee16ea
Fangsangik <hwangsangik@gmail.com>
2024-11-12 21:23:34 +0900

    feat : "BaseEntity" - 생성 날짜와, 수정날짜 공통 처리

---
+++ src/main/java/com/example/scheduledevelopment/util/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.scheduledevelopment.util;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
