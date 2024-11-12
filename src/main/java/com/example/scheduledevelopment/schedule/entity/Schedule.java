4946cfbaefa6af1f8ac25fc97d485a3ab7c0c758
Fangsangik <hwangsangik@gmail.com>
2024-11-12 21:36:11 +0900

    feat : "Schedule" - 스케줄 엔티티 - update 기능 구현

---
+++ src/main/java/com/example/scheduledevelopment/schedule/entity/Schedule.java
@@ -0,0 +1,53 @@
+package com.example.scheduledevelopment.schedule.entity;
+
+import com.example.scheduledevelopment.comment.entity.Comment;
+import com.example.scheduledevelopment.member.entity.Member;
+import com.example.scheduledevelopment.util.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String author;
+    private String title;
+    private String todo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+
+    public void updateAuthor(String author) {
+        this.author = author;
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public void updateMember(Member member) {
+        this.member = member;
+    }
+}
