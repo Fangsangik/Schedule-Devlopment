f25f289efcb80bf315d9645d2d0498316fd3b66d
Fangsangik <hwangsangik@gmail.com>
2024-11-13 13:08:46 +0900

    refactor : "ScheduleService" - 서비스 인터페이스화

--- src/main/java/com/example/scheduledevelopment/comment/service/CommentService.java
+++ src/main/java/com/example/scheduledevelopment/comment/service/CommentService.java
@@ -1,93 +1,12 @@
 package com.example.scheduledevelopment.comment.service;
 
 import com.example.scheduledevelopment.comment.dto.CommentDto;
-import com.example.scheduledevelopment.comment.entity.Comment;
-import com.example.scheduledevelopment.comment.mapper.CommentMapper;
-import com.example.scheduledevelopment.comment.repositroy.CommentRepository;
-import com.example.scheduledevelopment.member.dto.MemberDto;
-import com.example.scheduledevelopment.member.mapper.MemberMapper;
-import com.example.scheduledevelopment.member.service.MemberService;
-import com.example.scheduledevelopment.schedule.dto.ScheduleDto;
-import com.example.scheduledevelopment.schedule.entity.Schedule;
-import com.example.scheduledevelopment.schedule.mapper.ScheduleMapper;
-import com.example.scheduledevelopment.schedule.service.ScheduleService;
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class CommentService {
-
-    private final CommentRepository commentRepository;
-    private final CommentMapper commentMapper;
-    private final ScheduleMapper scheduleMapper;
-    private final ScheduleService scheduleService;
-
-    public CommentService(CommentRepository commentRepository, CommentMapper commentMapper,
-                          ScheduleMapper scheduleMapper, ScheduleService scheduleService) {
-        this.commentRepository = commentRepository;
-        this.commentMapper = commentMapper;
-        this.scheduleMapper = scheduleMapper;
-        this.scheduleService = scheduleService;
-    }
-
-    //create
-    @Transactional
-    public CommentDto createComment(CommentDto commentDto) {
-
-        ScheduleDto schedule = commentDto.getScheduleDto() != null
-                ? scheduleService.findScheduleById(commentDto.getScheduleDto().getId())
-                : null;
-
-        Comment comment = Comment.builder()
-                .commentDetails(commentDto.getCommentDetails())
-                .schedule(scheduleMapper.toEntity(schedule))
-                .build();
-
-        Comment createdComment = commentRepository.save(comment);
-        return commentMapper.toDto(createdComment);
-    }
-
-    //update
-    public CommentDto updateComment(Long id, CommentDto commentDto) {
-        Comment comment = validateId(id);
-
-        if (commentDto.getCommentDetails() != null) {
-            comment.updateCommentDetails(commentDto.getCommentDetails());
-        }
-
-        if (commentDto.getScheduleDto() != null) {
-            Schedule schedule = scheduleMapper.toEntity(
-                    scheduleService.findScheduleById(commentDto.getScheduleDto().getId())
-            );
-            comment.updateSchedule(schedule);
-        }
-
-        // JPA는 영속성 컨텍스트 내에서 엔티티의 변경 사항을 자동으로 추적하고 저장
-        return commentMapper.toDto(comment);
-    }
-
-    //조회
-    @Transactional(readOnly = true)
-    public CommentDto getCommentById(Long id) {
-        Comment comment = validateId(id);
-//        if (comment.getMember() != null) {
-//            comment.getMember().getId(); // Lazy 로딩 강제 초기화
-//        }
-        return commentMapper.toDto(comment);
-    }
-
-    //삭제
-    @Transactional
-    public void deleteCommentById(Long id) {
-        validateId(id);
-        commentRepository.deleteById(id);
-    }
-
-    private Comment validateId(Long id) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 comment을 찾을 수 없습니다."));
-
-        return comment;
-    }
+//인터페이스만 바라봐도 되는것
+//다른 구현체를 만든다 하더라도 만들어서 interface로 만들어 놓으면 바꿀 필요 없음
+public interface CommentService {
+    CommentDto createComment(CommentDto commentDto);
+    CommentDto updateComment(Long id, CommentDto commentDto);
+    CommentDto getCommentById(Long id);
+    void deleteCommentById(Long id);
 }
--- src/main/java/com/example/scheduledevelopment/member/service/MemberService.java
+++ src/main/java/com/example/scheduledevelopment/member/service/MemberService.java
@@ -1,101 +1,19 @@
 package com.example.scheduledevelopment.member.service;
 
-import com.example.scheduledevelopment.config.PasswordEncoder;
-import com.example.scheduledevelopment.member.entity.Member;
-import com.example.scheduledevelopment.member.mapper.MemberMapper;
 import com.example.scheduledevelopment.member.dto.MemberDto;
-import com.example.scheduledevelopment.member.repository.MemberRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * interface로 가져간 이유
+ * IoC와 DI를 생각해보면 interface로 가져가는게 좋다고 생각이 들었다.
+ * 하지만 작은 프로젝트의 경우 비용적인 면에서 불리하다고 생각이 들기도 한다.
+ * 그럼에도 불구하고 항상 service.class 이렇게 가져가는 것 보다는 IoC를 지켜보고 싶어 이렇게
+ * 적용해봤음
+ */
+public interface MemberService {
+    MemberDto createMember(MemberDto memberDto);
 
-@Service
-public class MemberService {
+    MemberDto updateMember(Long id, MemberDto memberDto);
 
-    private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final MemberMapper memberMapper;
-
-    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, MemberMapper memberMapper) {
-        this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.memberMapper = memberMapper;
-    }
-
-
-    //create
-    @Transactional
-    public MemberDto createMember(MemberDto memberDto) {
-        memberDto = encryptedPassword(memberDto);
-
-        Member member = memberMapper.toEntity(memberDto);
-        Member createMember = memberRepository.save(member);
-        return memberMapper.toDto(createMember);
-    }
-
-    //update
-    @Transactional
-    public MemberDto updateMember(Long id, MemberDto memberDto) {
-        Member findMember = validateId(id);
-
-        // 비밀번호 암호화 (필요한 경우)
-        memberDto = encryptedPassword(memberDto);
-
-        // 기존 엔티티의 필드 업데이트 (setter 사용 없이 필드를 직접 수정하는 방식)
-        if (memberDto.getName() != null) {
-            findMember.updateName(memberDto.getName()); // 엔티티 메서드로 필드 수정
-        }
-        if (memberDto.getEmail() != null) {
-            findMember.updateEmail(memberDto.getEmail()); // 엔티티 메서드로 필드 수정
-        }
-        if (memberDto.getPassword() != null) {
-            findMember.updatePassword(memberDto.getPassword()); // 엔티티 메서드로 필드 수정
-        }
-
-        // JPA는 영속성 컨텍스트 내에서 엔티티의 변경 사항을 자동으로 추적하고 저장
-        return memberMapper.toDto(findMember);
-
-    }
-
-    //조회
-    @Transactional(readOnly = true)
-    public MemberDto findMemberById(Long id) {
-        Member findMember = validateId(id);
-        return memberMapper.toDto(findMember);
-    }
-
-    //삭제
-    @Transactional
-    public void deleteMemberById(Long memberId) {
-        validateId(memberId);
-        // 회원 삭제
-        memberRepository.deleteById(memberId);
-    }
-
-
-    public boolean authenticate(String email, String password) {
-        Member member = memberRepository.findByEmail(email);
-
-        if (member != null && passwordEncoder.matches(password, member.getPassword())) {
-            return true;
-        }
-
-        return false;
-    }
-
-
-    private Member validateId(Long id) {
-        Member findMemberId = memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
-
-        return findMemberId;
-    }
-
-    private MemberDto encryptedPassword(MemberDto memberDto) {
-        if (memberDto.getPassword() != null && !memberDto.getPassword().isEmpty()) {
-            String encodedPassword = passwordEncoder.encode(memberDto.getPassword());
-            memberDto = memberDto.withPassword(encodedPassword);
-        }
-        return memberDto;
-    }
+    MemberDto findMemberById(Long id);
+    void deleteMemberById(Long memberId);
 }
