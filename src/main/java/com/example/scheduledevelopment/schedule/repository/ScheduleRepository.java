7c078c02da22141fd33b877c8194176c20ac2057
Fangsangik <hwangsangik@gmail.com>
2024-11-13 11:49:57 +0900

    refactor : "ScheduleRepository" - findAll에서 EntityGraph 부분 삭제

--- src/main/java/com/example/scheduledevelopment/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduledevelopment/schedule/repository/ScheduleRepository.java
@@ -3,11 +3,8 @@ package com.example.scheduledevelopment.schedule.repository;
 import com.example.scheduledevelopment.schedule.entity.Schedule;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    //N+1 문제를 방지하기 위해 @EntityGraph를 사용하여 연관된 엔티티를 페치
-    @EntityGraph(attributePaths = {"comments", "member"})
     Page<Schedule> findAll(Pageable pageable);
 }
