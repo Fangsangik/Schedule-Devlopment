83763002a2094077ebc37ab59cece1953b4f8d14
Fangsangik <hwangsangik@gmail.com>
2024-11-14 10:55:06 +0900

    refactor : "BaseEntity" - column name 일치

--- src/main/java/com/example/scheduledevelopment/common/BaseEntity.java
+++ src/main/java/com/example/scheduledevelopment/common/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.example.scheduledevelopment.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,9 +18,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class) //Auditing을 사용하겠다고 선언
 public abstract class BaseEntity {
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
